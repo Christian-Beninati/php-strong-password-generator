eb8a2f6444c302120b071a713773c500ac66f04a
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-19 16:15:21 +0100

    form

--- index.php
+++ index.php
@@ -17,7 +17,21 @@ $page_title = 'PHP Strong Password Generator';
 </head>
 
 <body>
-
+    <main class="container text-center mt-5">
+        <h1>Strong Password Generator</h1>
+        <section id="password_generator">
+            <form action="" method="GET">
+                <div class=" mt-4">
+                    <label for="length">Password length:</label>
+                    <input type="number" id="length" name="length" min="1" max="20" required>
+                </div>
+                <div class="mt-5">
+                    <button class="btn btn-primary ms-4" type="submit" name="generate">Send</button>
+                    <button class="btn btn-secondary" type="reset" name="cancel">Cancel</button>
+                </div>
+            </form>
+        </section>
+    </main>
 </body>
 
 </html>
