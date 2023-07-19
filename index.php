61f7b489e8b496d51d54eb2c24d484f64e8df067
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-19 14:03:14 +0100

    bootstrap

--- index.php
+++ index.php
@@ -11,6 +11,9 @@ $page_title = 'PHP Strong Password Generator';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page_title ?? 'Document' ?></title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
