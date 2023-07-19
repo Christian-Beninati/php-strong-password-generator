f6849e05dbd614ddbe8adf5dd35aaf7bd892d45f
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-19 14:02:46 +0100

    page_title

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+$page_title = 'PHP Strong Password Generator';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $page_title ?? 'Document' ?></title>
+</head>
+
+<body>
+
+</body>
+
+</html>
